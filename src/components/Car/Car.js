2ca681a16b1aaeafb62b33a160669962cca341b2
Md Najmul Haque <najmulhaque008@gmail.com>
2022-12-05 20:58:18 +0600

    complete card alignment

--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Car = ({ car }) => {
-    const { _id, carName, mileage, location, engine, transmission, price } = car
+    const { _id, carName, mileage, location, engine, transmission, img, price } = car
     return (
-        <div>
 
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="px-10 pt-10">
+                <h2 className="card-title pb-5">{carName}</h2>
+                <figure>
+                    <img src={img} alt="img" />
+                </figure>
+            </div>
+            <div className="card-body">
+
+                <div className='flex justify-between'>
+                    <p>Mileage: {mileage}</p>
+                    <p>Location: {location}</p>
+                </div>
+                <div className='flex justify-between'>
+                    <p>Engine: {engine}</p>
+                    <p>Transmission: {transmission}</p>
+                </div>
+                <div className='flex justify-between'>
+                    <p>Price: {price}</p>
+                    <Link to='' className="btn btn-error btn-sm px-5">More details</Link>
+                </div>
+
+            </div>
         </div>
+
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Car from '../Car/Car';
 import Loading from '../Shared/Loading';
 
 const Home = () => {
 
     const { data: cars, isLoading, refetch } = useQuery(['cars'], () =>
-        fetch('http://localhost:5000/api/v1/car')
+        fetch('http://localhost:5001/api/v1/car')
             .then(res => res.json())
     )
 
@@ -18,6 +19,8 @@ const Home = () => {
     //         )
     // })
 
+    refetch();
+
     console.log(cars)
 
     if (isLoading) {
@@ -27,11 +30,35 @@ const Home = () => {
 
 
     return (
-        <>
-            {
-                cars.result.map(car => <Car key={car._id} car={car} />)
-            }
-        </>
+        <div className="drawer drawer-mobile">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+                <div className='grid grid-cols-1 md:grid-cols-3 mx-auto gap-3'>
+                    {
+                        cars.result.map(car => <Car key={car._id} car={car} />)
+                    }
+                </div>
+
+                {/* <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label> */}
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                    <li><Link to=''>Basic Information</Link></li>
+                    <li><Link to=''>Car Cost</Link></li>
+                    <li><Link to=''>Search Region</Link></li>
+                    <li><Link to=''>Vehicle Cost</Link></li>
+                    <li><Link to=''>Add. Characteristics</Link></li>
+                    <li><Link to=''>Color</Link></li>
+                    <li><Link to=''>Car Cost</Link></li>
+                </ul>
+
+            </div>
+        </div>
+
+
+
+
     );
 };
 
